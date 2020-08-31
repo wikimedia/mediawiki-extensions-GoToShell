ff158c172c63716371ef281b160fc4e6a259cce8
zoranzoki21 <zorandori4444@gmail.com>
2020-08-31 07:37:28 +0200

    SpecialGoToShell.php: Convert indentation to use tabs, instead of spaces
    Change-Id: Idd65f7e93c28726ade6cfc3d9554bf3ac05e1bad

--- SpecialGoToShell.php
+++ SpecialGoToShell.php
@@ -3,63 +3,63 @@
 use MediaWiki\Shell\Shell;
 
 class SpecialGoToShell extends SpecialPage {
-   function __construct( ) {
-      parent::__construct( 'GoToShell' );
-   }
+	function __construct( ) {
+		parent::__construct( 'GoToShell' );
+	}
 
-   function execute( $par ) {
-      global $wgGoToShellCommand;
-      $user = $this->getUser();
-      // Missing handbasket, er, permission
-      if ( !$user->isAllowed( 'gotoshell' ) ) {
-         // Shell no, this user won't go
-         throw new PermissionsError( null, array( array(
-            'gotoshell-notallowed' ) ) );
-      }
-      $this->setHeaders();
-      $this->outputWikiText( "<big>'''" . wfMessage( 'gotoshell-command' )
-         . "'''</big><br>$wgGoToShellCommand<br><br>"
-         . "<big>'''" . wfMessage ( 'gotoshell-result' ) . "'''</big><br>" );
-      // To shell with this user!
-      // Some shell commands (ex. mediawiki maintenance scripts) might write to both stdout and
-      // stderr. Capture and output both, with errors before normal output for greater visibility.
-      if ( Shell::isDisabled() ) {
-         $this->outputWikiText( wfMessage( 'gotoshell-disabled' ) );
-      } else {
-         $result = Shell::command( [] )
-            ->unsafeParams( $wgGoToShellCommand )
-            ->restrict( Shell::RESTRICT_NONE )
-            ->execute();
+	function execute( $par ) {
+		global $wgGoToShellCommand;
+		$user = $this->getUser();
+		// Missing handbasket, er, permission
+		if ( !$user->isAllowed( 'gotoshell' ) ) {
+			// Shell no, this user won't go
+			throw new PermissionsError( null, array( array(
+				'gotoshell-notallowed' ) ) );
+		}
+		$this->setHeaders();
+		$this->outputWikiText( "<big>'''" . wfMessage( 'gotoshell-command' )
+			. "'''</big><br>$wgGoToShellCommand<br><br>"
+			. "<big>'''" . wfMessage ( 'gotoshell-result' ) . "'''</big><br>" );
+		// To shell with this user!
+		// Some shell commands (ex. mediawiki maintenance scripts) might write to both stdout and
+		// stderr. Capture and output both, with errors before normal output for greater visibility.
+		if ( Shell::isDisabled() ) {
+			$this->outputWikiText( wfMessage( 'gotoshell-disabled' ) );
+		} else {
+			$result = Shell::command( [] )
+				->unsafeParams( $wgGoToShellCommand )
+				->restrict( Shell::RESTRICT_NONE )
+				->execute();
 
-         $this->processOutput( $result->getStderr() );
-         $this->processOutput( $result->getStdout() );
-      }
-   }
+			$this->processOutput( $result->getStderr() );
+			$this->processOutput( $result->getStdout() );
+		}
+	}
 
-   /**
-    * @param $rawOutput
-    * @throws MWException
-    */
-   protected function processOutput( $rawOutput ) {
-      if ( $rawOutput ) {
-         foreach ( explode( "\n", $rawOutput ) as $line ) {
-            $this->outputWikiText( $line );
-         }
-      }
-   }
+	/**
+	 * @param $rawOutput
+	 * @throws MWException
+	 */
+	protected function processOutput( $rawOutput ) {
+		if ( $rawOutput ) {
+			foreach ( explode( "\n", $rawOutput ) as $line ) {
+				$this->outputWikiText( $line );
+			}
+		}
+	}
 
-   private function outputWikiText( $wikitext ) {
-      $output = $this->getOutput();
-      if ( method_exists( $output, 'addWikiTextAsInterface' ) ) {
-         // MW 1.32+
-         $output->addWikiTextAsInterface( $wikitext );
-      } else {
-         $output->addWikiText( $wikitext );
-      }
-   }
+	private function outputWikiText( $wikitext ) {
+		$output = $this->getOutput();
+		if ( method_exists( $output, 'addWikiTextAsInterface' ) ) {
+			// MW 1.32+
+			$output->addWikiTextAsInterface( $wikitext );
+		} else {
+			$output->addWikiText( $wikitext );
+		}
+	}
 
-   protected function getGroupName() {
-      return 'other';
-   }
+	protected function getGroupName() {
+		return 'other';
+	}
 }
 
